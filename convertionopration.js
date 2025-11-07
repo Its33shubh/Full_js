9916e7510b5704aec66a61cc96889b75544d45b7
Its33shubh <shubhamkakloter123@gmail.com>
2025-11-07 19:18:04 +0530

    opration done

--- convertionopration.js
+++ convertionopration.js
@@ -26,3 +26,27 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 // 1 => true; 0 => false
 // "" => false
 // "hitesh" => true
+
+
+// *************************opration********************
+
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+
+// more over study about postfix and perfix
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
