8fec867035e2d52650ea34228ca8103bf88f82fb
Its33shubh <shubhamkakloter123@gmail.com>
2025-11-07 18:48:27 +0530

    do convertion

---
+++ convertionopration.js
@@ -0,0 +1,28 @@
+let age = null;
+
+console.log(typeof age);
+let valueOfAge = Number(age);
+console.log(valueOfAge);
+/* 
+        "33" => 33
+        "33abc" => NaNF
+         true => 1; false => 0
+*/
+
+
+let name = 145;
+console.log(name);
+
+let valueOfName = String(name);
+
+
+console.log(valueOfName);
+
+let isLoggedIn = "hitesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
---
+++ var_let_const.js
@@ -0,0 +1,11 @@
+var name ="shubham"; // old way to declare a variable thay have globle scope and they also reasign and redecalre 
+
+
+let age = 21 // this introduce in es6(2015) this morden way and reasignment is allow but you don't redeclare it
+
+const rollNo = 2121; // it use to declare constant value means fix value and this can't change 
+city="bhavnagar"
+
+console.table([name,age,rollNo,city]);
+
+
